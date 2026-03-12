d9c0afd71ccc4674be3257a36fb22078d3adde8d
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-03-12 12:41:13 -0400

    This code on button "A" press measures distance using sonar.

--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@
  *
  * Created by: Matthew Carnaffan
  * Created on: Mar 2026
- * This program ...
+ * This program finds distance using sonar
 */
 
+let distanceSensor:number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//Find distance using distance sensor
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    distanceSensor = sonar.ping
+    (DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters)
+    basic.showNumber(distanceSensor)
+    basic.showIcon(IconNames.Happy)
+})
